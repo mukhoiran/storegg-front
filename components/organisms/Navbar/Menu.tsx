923cc147df7f12a56442dc8adb808db73271a590
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2022-02-16 11:21:12 +0700

    refactor navbar to organisms

---
+++ components/organisms/Navbar/Menu.tsx
@@ -0,0 +1,25 @@
+import cx from 'classnames';
+import Link from 'next/link';
+
+interface MenuProps {
+    title: string; //required
+    active?: boolean; //optional
+    href: string;
+}
+
+export default function Menu(props: Partial<MenuProps>) {
+  const { title, active, href='/' } = props;
+  
+  const classTitle = cx({
+    'nav-link': true,
+    'active': active
+  });
+
+  return (
+    <li className="nav-item my-auto">
+        <Link href={href}>
+            <a className={classTitle} aria-current="page" href="/#">{title}</a>
+        </Link>
+    </li>
+  )
+}
